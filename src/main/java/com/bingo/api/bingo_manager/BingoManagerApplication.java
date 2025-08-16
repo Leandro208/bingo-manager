26ac76683e9d3fb03c9b85e4138413b69dabcb75
Leandro Barbosa <leandro.barbosa@esig.com.br>
2025-08-16 12:55:43 -0300

    first commit

---
+++ src/main/java/com/bingo/api/bingo_manager/BingoManagerApplication.java
@@ -0,0 +1,13 @@
+package com.bingo.api.bingo_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BingoManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BingoManagerApplication.class, args);
+	}
+
+}
